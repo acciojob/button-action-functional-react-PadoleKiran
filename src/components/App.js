ac53f01be2b34974c1f0ac484fc08c49f4a953d2
Kiran <padolekiran2001@address.com>
2025-12-05 12:03:57 +0530

    task compleated

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,26 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [val,setVal] = useState(false);
+  // let [num, setNum] = useState(1);
+
+  // function ValState(){
+  //   // setNum(num+1);
+  //   let nextNum = num + 1;
+  //   setNum(nextNum);
+
+  //   if(num%2 === 0){
+  //     setVal(true);
+  //   }
+  //   else{
+  //     setVal(false);
+  //   }
+  // }
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={()=>setVal(true)}>Click me</button>
+      {val && (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>)}
     </div>
   );
 }
